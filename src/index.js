2ef236df6f115bae52af0516594d4ec2643fd996
Tossy Kim <dydrkfk22@gmail.com>
2024-08-29 16:26:13 +0900

    React 2장, 3장

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './chp03/Boook.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from "./chp03/Library";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Library/>
   </React.StrictMode>
 );
 
