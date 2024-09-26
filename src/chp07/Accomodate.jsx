533053544b1b94d597e58e05e8e502407c689c59
Tossy Kim <dydrkfk22@gmail.com>
2024-09-26 16:28:08 +0900

    React 7장 실습 useState, useEffect

---
+++ src/chp07/Accomodate.jsx
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from "react"
+import useCounter from "./useCounter";
+
+const MAX_CAPACITY = 10;
+
+function Accomodate(props){
+    const [isFull, setIsFull] = useState(false)
+    const [count, increasecount, decreasecount] = useCounter(0)
+
+    useEffect(() => {
+        console.log("======================");
+        console.log("useEffect 실행됨");
+        console.log(`isFull : ${isFull}`);
+    }, []);
+
+    useEffect(() => {
+        setIsFull(count >= MAX_CAPACITY)
+        console.log(`current count value : ${count}`);
+    }, [count]);
+
+    return(
+        <div style={{padding: 16}}>
+            <p>{`총 ${count}명 수용했습니다.`}</p>
+            <button onClick={increasecount} disabled={isFull}>수용시설에 입장</button>
+            <button onClick={decreasecount}>수용시설에서 퇴장.</button>
+            {isFull && <p style={{color: "red"}}>정원이 가득찼습니다.</p>}
+        </div>
+    );
+}
+
+export default Accomodate;
--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import NotificationList from "./chp06/NotificationList";
-import './chp06/Notification.css'
+import Accomodate from "./chp07/Accomodate";
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NotificationList/>
+    <Accomodate/>
   </React.StrictMode>
 );
 
